96e40d67f13c33207cd331ca396a8314cf2ebebf
seimpairiyun <m.afizha96@gmail.com>
2022-05-25 23:31:05 +0700

    feat: pembuatan struktur object

--- index.js
+++ index.js
@@ -4,17 +4,104 @@
 const firstUser = {};
 const secondUser = {};
 
+// ! FIRST USER
+firstUser.name = "Monica";
+firstUser.gender = "female";
+firstUser.age = 17;
+firstUser.email = "monica@dingdong.com";
+firstUser.favoriteColor = "Yellow, Pink, White, Purple";
+firstUser.isHavePet = "Yes";
+firstUser.education = [];
+
+firstUser.education.push(
+  {
+    name: "SD 01",
+    city: "Jakarta",
+    graduate: "2016",
+  },
+  {
+    name: "SMP 02",
+    city: "Jakarta",
+    graduate: "2019",
+  },
+  { name: "SMA 03", city: "Tangerang" }
+);
+
+firstUser.favoriteRestaurant = new Set();
+
+[
+  "Bento",
+  "Sushi",
+  "Pancake",
+  "Eggy",
+  "Tempura",
+  "Bento",
+  "Eggy",
+  "Padang",
+  "Tteok",
+  "Sushi",
+  "Sushi",
+].forEach((n) => {
+  firstUser.favoriteRestaurant.add(n);
+});
+
+// ! SECOND USER
+secondUser.name = "Wendy";
+secondUser.gender = "male";
+secondUser.age = 23;
+secondUser.email = "wendy@dingdong.com";
+secondUser.favoriteColor = "Blue, Black, Grey";
+secondUser.isHavePet = "No";
+secondUser.education = [];
+
+secondUser.education.push(
+  {
+    name: "SD 02",
+    city: "Jakarta",
+    graduate: 2010,
+  },
+  {
+    name: "SMP 03",
+    city: "Bogor",
+    graduate: 2013,
+  },
+  { name: "SMA 01", city: "Surabaya", graduate: 2016 },
+  { name: "Universitas Maju", city: "Tangerang" }
+);
+
+secondUser.favoriteRestaurant = new Set();
+
+[
+  "Tempura",
+  "Bento",
+  "Sushi",
+  "Pancake",
+  "Padang",
+  "Katsu",
+  "Geprek",
+  "Pancake",
+  "Eggy",
+].forEach((n) => {
+  secondUser.favoriteRestaurant.add(n);
+});
+
+//
+//
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
+users.push(firstUser, secondUser);
+
+//
+//
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
-    console.log(users.length || users.size);
-    console.log(users);
+  console.log(users.length || users.size);
+  console.log(users);
 }
 
 main();
 
 module.exports = {
-    users
+  users,
 };
